b1d1d95e2740ef2ae562910ebef8f552c47a6add
Bas van der Hoek <bvdhoek@zilverline.com>
2013-12-13 10:02:32 +0100

    apply bold style with regular expression

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -32,6 +32,40 @@ class MarkdownTextStorage < NSTextStorage
     self.endEditing
   end
 
+  def processEditing
+    puts self.editedRange.inspect
+    self.performReplacementsForRange(self.editedRange)
+    super
+  end
+
+  def performReplacementsForRange(changedRange)
+    extendedRange = NSUnionRange(changedRange, @backingStore.string.lineRangeForRange(NSMakeRange(changedRange.location, 0)))
+    #extendedRange = NSUnionRange(changedRange, @backingStore.string.lineRangeForRange(NSMakeRange(NSMaxRange(changedRange), 0)))
+    self.applyStylesToRange(extendedRange)
+  end
+
+  def applyStylesToRange(searchRange)
+    boldFont = NSFontManager.sharedFontManager.fontWithFamily("Avenir Next", traits: NSBoldFontMask, weight: 0, size: 17)
+    normalFont = NSFont.fontWithName("Avenir Next", size: 17)
+
+    regexStr = "(\\*\\w+(\\s\\w+)*\\*)\\s"
+    regex = NSRegularExpression.regularExpressionWithPattern(regexStr, options: 0, error: nil)
+
+    boldAttributes = {NSFontAttributeName => boldFont}
+    normalAttributes = {NSFontAttributeName => normalFont}
+
+    regex.enumerateMatchesInString(@backingStore.string, options: 0, range: searchRange,
+      usingBlock: lambda do |match, flags, stop|
+        puts match.inspect
+        matchRange = match.rangeAtIndex(1)
+        self.addAttributes(boldAttributes, range: matchRange)
+        if (NSMaxRange(matchRange) + 1 < self.length)
+          self.addAttributes(normalAttributes, range: NSMakeRange(NSMaxRange(matchRange) + 1, 1))
+        end
+      end
+    )
+  end
+
 end
 
 class AppDelegate
